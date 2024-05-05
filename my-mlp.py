0829dc0fe63e23b08cb81654fd7a7f3291071975
Mike Millar <mmillar@unm.edu>
2024-05-05 10:33:03 -0600

    update my-mlp parameters and added comments

--- my-mlp.py
+++ my-mlp.py
@@ -14,52 +14,100 @@ training_file = 'data/train/music_20_mfcc.csv'
 testing_file = 'data/test/test_20_mfcc.csv'
 # Parameters
 batch_size = 60
+max_epochs = 100
+learning_rate = 0.0001
+
+# Define layer sizes
+# 1st element is input, last element is output
+# Each row contains 20 elements, output is 10 classes
+# NOTE: Currently only accepts 3 values
+layer_sizes = [20, 1028, 10]
+
+# Define activation, cost, optimzer functions
+activation = nn.ReLU()
+cost = nn.CrossEntropyLoss()
+optimizer = 'Adam'
 
 class CustomDataset(Dataset):
+    """
+    Custom implementation dataset for use with our pandas dataframe
+    """
     def __init__(self, data, labels):
+        """
+        Initializes the dataset with our data and labels.
+
+        Args:
+            data (dataframe): Dataframe of data
+            labels (list): List of labels
+        """
         self.data = data
         self.labels = labels
 
     def __getitem__(self, index):
+        """
+        Used by data loader to extract batches of data
+
+        Args:
+            index (int): Index of data
+
+        Returns
+            (arraylike, str): Returns arraylike of data and string label
+        """
         row = self.data[index]
         label = self.labels[index]
         return row, label
     
     def __len__(self):
+        """
+        Outputs the total size of the dataset
+
+        Returns
+            (int): Size of the dataset
+        """
         return len(self.data)
     
 class MLP(nn.Module):
+    """
+    Custom implementation of our multi-layer perceptron using PyTorch
+    """
+
     def __init__(self, layer_sizes, activation=nn.ReLU(), cost=nn.CrossEntropyLoss(), optimizer='SGD', dropout_rate=0.2, learning_rate=1e-3):
+        """
+        Initializes the network.
+
+        Args:
+            layer_sizes (arraylike): An arraylike object of layer sizes.
+              This is currently hard coded to only accept total length of 3.
+              The first element is the input size, second element is the
+              hidden layer size, and the final element is the output size.
+            activation (function): PyTorch activation function. Defaulted to ReLU()
+            cost (function): PyTorch cost(loss) function. Defaulted to CrossEntropyLoss()
+            optimizer (str): String representation of PyTorch optimizer to use. Valid
+              options include: 'SGD', and 'Adam'
+            dropout_rate (float): Float valued between 0 and 1, the dropout rate between layers.
+            learning_rate (float): Learning rate used in parameter training.
+        """
         super(MLP, self).__init__()
+        # Initialize variables
         self.learning_rate = learning_rate
         self.activation = activation
         self.cost = cost
         self.dropout = nn.Dropout(dropout_rate)
         self.layers = nn.ModuleList()
-        # layers = []
+        # Create layers
         for i in range(len(layer_sizes)-1):
             layer = nn.Linear(layer_sizes[i], layer_sizes[i+1])
             self.layers.append(layer)
-            # layers.append(layer)
-            # if i < len(layer_sizes) - 2:
-            #     layers.append(self.activation)
-            #     layers.append(self.dropout)
-            # else:
-            #     layers.append(nn.Softmax())
-        # self.linear_relu_stack = nn.Sequential(*layers)
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(layer_sizes[0], layer_sizes[1]),
-            nn.ReLU(),
-            nn.Dropout(dropout_rate),
-            nn.Linear(layer_sizes[1], layer_sizes[1]),
-            nn.ReLU(),
-            nn.Dropout(dropout_rate),
-            nn.Linear(layer_sizes[1], layer_sizes[2])
-        )
+        # Create optimizer
         self.set_optimizer(optimizer)
 
     def forward(self, x):
-        # return self.linear_relu_stack(x)
+        """
+        Internally used method of PyTorch for training, validation, and prediction.
+
+        Args:
+            x (Tensor): Tensor data
+        """
         z = x
         for i in range(len(self.layers)):
             layer = self.layers[i]
@@ -70,7 +118,13 @@ class MLP(nn.Module):
 
     
     def set_optimizer(self, optimizer):
-        # TODO: Add more optimizer options
+        """
+        Utility method to create the optimizer used for parameter training.
+
+        Args:
+            optimizer (str): String identifier of optimizer to use. Valid options
+              are 'SGD' and 'Adam'.
+        """
         if optimizer == 'SGD':
             self.optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate)
         elif optimizer == 'Adam':
@@ -78,9 +132,32 @@ class MLP(nn.Module):
         
 
 def load_data(file):
+    """
+    Helper method to load pandas dataframe from file.
+
+    Args:
+        file (str): File path of dataframe
+
+    Returns:
+        (dataframe): Pandas dataframe
+    """
     return pd.read_csv(file, index_col=0)
 
 def split_data(X):
+    """
+    Method to split the data into training and validation steps. 
+    Additionally performs a mapping of the labels to integers.
+
+    Args:
+        X (dataframe): Dataframe of data to split
+
+    Returns:
+        (dataframe): Feature training data
+        (dataframe): Feature validation data
+        (dataframe): Label training data
+        (dataframe): Label validation data
+        (dict): Mapping dictionary for mapping labels back to strings
+    """
     Y, classes = pd.factorize(X['label'])
     mapping = dict(zip(range(len(classes)), classes))
     print(f'classes={classes}, mapping={mapping}')
@@ -89,6 +166,15 @@ def split_data(X):
     return X_train, X_test, Y_train, Y_test, mapping
 
 def normalize_data(X_train, X_test, Z):
+    """
+    Helper method to transform the data into normalized form,
+    with mean of zero, and standard deviation of 1.
+
+    Args:
+        X_train (dataframe): Training data
+        X_test (dataframe): Validation data
+        Z (dataframe): Competition data
+    """
     scaler = MinMaxScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
@@ -96,6 +182,21 @@ def normalize_data(X_train, X_test, Z):
     return X_train, X_test, Z
 
 def load_split_normalize(train_file, test_file):
+    """
+    Helper method to load, split and normalize the data. The method
+    additionally converts the pandas dataframes into PyTorch datasets,
+    and creates data loaders.
+
+    Args:
+        train_file (str): Path to training data
+        test_file (str): Path to competition data
+
+    Returns:
+        (dataloader): Training data laoder
+        (dataloader): Validation data loader
+        (dataloader): Competition data loader
+        (dict): Mapping of labels from str to int
+    """
     # Load the data
     train = load_data(train_file)
     test = load_data(test_file)
@@ -111,6 +212,13 @@ def load_split_normalize(train_file, test_file):
     return train_loader, test_loader, Z, mapping
 
 def train(dataloader, model):
+    """
+    Method used to train an individual epoch for the MLP model.
+
+    Args:
+        dataloader (dataloader): Data loader of the training data
+        model (nn.Module): ML model to train
+    """
     size = len(dataloader.dataset)
     model.train()
     for batch, (X, y) in enumerate(dataloader):
@@ -129,6 +237,16 @@ def train(dataloader, model):
         # print(f'loss: {loss:>7f}   [{current:>5d}/{size:>5d}]')
 
 def test(dataloader, model):
+    """
+    Method used to validate an individual epoch for the MLP model.
+
+    Args:
+        dataloader (dataloader): Data loader of the validation data
+        model (nn.Module): ML model to train
+
+    Returns:
+        (float, float): Epoch accuracy and average loss
+    """
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
@@ -159,21 +277,10 @@ if __name__ == "__main__":
     )
     print(f'Using {device} device')
 
-    # Define layer sizes
-    # 1st element is input, last element is output
-    # Each row contains 20 elements, output is 10 classes
-    layer_sizes = [20, 1028, 10]
-
-    # Define activation, cost functions
-    activation = nn.ReLU()
-    cost = nn.CrossEntropyLoss()
-
     # Create model
-    model = MLP(layer_sizes, activation=activation, cost=cost, optimizer='Adam', learning_rate=0.001).to(device)
+    model = MLP(layer_sizes, activation=activation, cost=cost, optimizer=optimizer, learning_rate=learning_rate).to(device)
     print(model)
 
-    # Define number of training epochs
-    max_epochs = 500
     loss = float('inf')
     acc = 0.0
     for t in range(max_epochs):
