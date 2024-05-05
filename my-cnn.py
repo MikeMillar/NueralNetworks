72d06ce9185620366ae4cfe9650a56870b74a039
Mike Millar <mmillar@unm.edu>
2024-05-05 04:21:20 -0600

    restructure cnn architecture

--- my-cnn.py
+++ my-cnn.py
@@ -60,20 +60,22 @@ class GenreClassifier(nn.Module):
         # )
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64*56*56, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 10)
+        # self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        # self.fc1 = nn.Linear(64*56*56, 512)
+        self.fc1 = nn.Linear(32*112*112, 256)
+        self.fc2 = nn.Linear(256, 10)
+        # self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
         # return self.network(x.float())        
         x = x.float()
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        # x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        # x = F.relu(self.fc2(x))
+        x = self.fc2(x)
+        # x = self.fc3(x)
         return x
     
 def load_data(dir, hasLabels):
